3a2c5515e1bdf05637d8565547222229ba6682db
marianavns <mariana.vns@gmail.com>
2021-03-08 17:24:58 -0300

    feat: average consumption per segment

--- 32-gasoline.c
+++ 32-gasoline.c
@@ -6,30 +6,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
-int main () {
+int main (int argc, char *argv[]) {
     float 
-        totalKm, 
-        l, 
-        totalL, 
-        results;
+        totalKm, totalL, supplyL[200], traveledKm[200], average[200];
     int 
-        gasStations, 
-        counter;
+        i, s, gasStations;
 
     printf("Voce abasteceu em quantos postos de gasolina? ");
     scanf("%i", &gasStations);
-
-    for (counter = 1; counter <= gasStations; counter++) {
-        printf("Quantos litros abastecidos no posto %d? ", counter);
-        scanf("%f", &l);
-        totalL += l;
+   
+    for (s = 0; s < gasStations; s++) {
+        printf("Quantos litros abastecidos no posto %d? ", s + 1);
+        scanf("%f", &supplyL[s]);
+        printf("Quantos km rodados com este abastecimento? ");
+        scanf("%f", &traveledKm[s]);
+        average[s] = traveledKm[s]/supplyL[s];       
     }
-        
-    printf("Quantos quilometros dirigidos? ");
-    scanf("%f", &totalKm);
-    
-    results = totalKm/totalL;
-    printf("O carro consegue percorrer %.2f quilometros a cada litro.\n", results);
+    printf("Km rodados - litros abastecidos - consumo\n");
+    for (i = 0; i < gasStations; i++){
+        printf("%.1f km | %.1f l | %.2f km/l\n", traveledKm[i], supplyL[i], average[i]);
+    }
 
+//     char *star = "*";
+//     int i, s, dimensao;
+
+//     printf("Qual o tamanho do quadrado? ");
+//     scanf("%d", &dimensao);
+
+//     for (i = 0; i < dimensao; i++) {
+//         for(s = 0; s < dimensao; s++){
+//             printf(star);
+//         }
+//         printf("\n");
+//     }
+
+//     }
+     // printf("Quantos quilometros dirigidos? ");
+    // scanf("%f", &totalKm);
+    
+    // results = totalKm/totalL;
+    // printf("O carro consegue percorrer %.2f quilometros a cada litro.\n", results);
+
+return 0;  
 }
